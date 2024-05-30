4b1e4913a0a4183be62c77a397e109d7b6fec49b
Johannes Schöneberger <hannes1033@gmail.com>
2024-05-31 01:26:59 +0200

    Rewrote most of reload_addon function. - Now tries to save preferences - Checks for modules in .py files and directories - Uses addon:utils module to get all user addons - More error checking and notifying of user - Some logging

--- README.md
+++ README.md
@@ -13,5 +13,3 @@ blender -b -P reload_addons.py
 ## Usage
 
 This addon provides two commands, "Reload All Addons" and "Reload Enabled Addons" which can be accessed from the Blender menu in the topbar under System, or through the tool search bar. This will search the user addons directory (default `~/src/Blender`, configurable in Preferences -> Addons -> Reload Addons) for addon sources, install them and reload them in Blender's interpreter.
-
-The Reloader will call the shell script `package_addon.sh` before reloading. This can be used to perform whatever build actions you may need.
--- reload_addons.py
+++ reload_addons.py
@@ -3,59 +3,109 @@ import os
 import subprocess
 import sys
 from pathlib import Path
+import shutil
+
+# Not part of bpy API, but can still be used
+import addon_utils
 
 bl_info = {
     "name": "Reload Addons",
     "author": "JoeJoeTV, John Kanji",
-    "version": (1, 0, 0),
+    "description": "Adds the ability to update an in-development addon from a source directory and reload it without restarting blender",
+    "version": (1, 1, 0),
     "blender": (4, 0, 0),
     "location": "SpaceBar Search -> Reload All Addons",
     "category": "Development",
+    "support": "COMMUNITY",
 }
 
 
 def reload_addons(op, only_enabled=True):
+    """Installs and reloads addons from source path set in the addon preferences
+    
+    Arguments:
+        op: Addon operator used for outputting messages to Blender info panel
+        [only_enabled]: Whether only enabled addons should be tried for reloading
+    """
+
     preferences = bpy.context.preferences
     addon_prefs = preferences.addons[__name__].preferences
-    user_addons_path = addon_prefs.source_dir
+    user_addons_path = Path(addon_prefs.source_dir)
 
-    if not os.path.isdir(user_addons_path):
-        op.report({'ERROR'}, "The specified 'source dir' path is not a directory or doesn't exist!")
+    if not user_addons_path.is_dir():
+        op.report({'ERROR'}, f"The specified source directory path ({str(user_addons_path)}) is not a directory or doesn't exist!")
         return
 
     blender_addons_path = Path(bpy.utils.script_path_user())/'addons'
 
-    enabled_addons = [a.module for a in bpy.context.preferences.addons]
-    addons = next(os.walk(user_addons_path))[1]
-    addons = [a for a in addons if a[0] != '_']
+    # get list of all *user* installed addons and the enabled ones
+    addons = [m.__name__ for m in addon_utils.modules() if (str(blender_addons_path) in m.__file__)]
+    enabled_addons = [a.module for a in bpy.context.preferences.addons if a.module in addons]
 
     if only_enabled:
-        addons = [a for a in addons if a in enabled_addons]
+        addons = enabled_addons
 
-    cwd = os.getcwd()
     for a in addons:
-        op.report({'INFO'}, f'reloading {a}')
-        adir = os.path.join(user_addons_path, a)
-        os.chdir(adir)
-        if 'package_addon.sh' in os.listdir(adir):
-            op.report({'INFO'}, f'Running package_addon.sh for addon {a}')
-            subprocess.run([os.path.join(adir, 'package_addon.sh')])
+        addon_path = None
+
+        if (user_addons_path / f"{a}.py").is_file():
+            addon_path = user_addons_path / f"{a}.py"
+        elif (user_addons_path / a / "__init__.py").is_file():
+            addon_path = user_addons_path / a
+        else:
+            # If there is no python module as a file or directory, we can't install/reload addon, so try next one
+            op.report({'DEBUG'}, f"No source for addon '{a}' found in source path.")
+            continue
         
-        op.report({'INFO'}, f'Copying addon source code from {str(adir)} to {blender_addons_path}')
-        cmd = ['rsync', '-avh', adir, blender_addons_path]
-        op.report({'INFO'}, f'Running command {str(cmd)}')
-        subprocess.run(cmd)
+        # Save preferences of addon, since they get reset when disabling and re-enabling
+        addon_prefs = {}
+
+        if bpy.context.preferences.addons[a].preferences:
+            addon_prefs = dict(bpy.context.preferences.addons[a].preferences.items())
+
+        op.report({'DEBUG'}, f"Copying source code for addon '{a}'...")
+
+        try:
+            if addon_path.is_file():
+                shutil.copy2(addon_path, blender_addons_path / f"{a}.py")
+            elif addon_path.is_dir():
+                shutil.copytree(addon_path, blender_addons_path / a, dirs_exist_ok=True)
+            else:
+                op.report({'ERROR'}, f"Addon source is neither file not directory! How did you get here?")
+        except (EnvironmentError, IOError) as e:
+                op.report({'ERROR'}, f"Error while copying source code for addon '{a}'")
+                continue
+
+        op.report({'DEBUG'}, f"Reloading addon '{a}'...")
+
+        bpy.ops.preferences.addon_refresh()
 
         if a in enabled_addons:
-            bpy.ops.preferences.addon_disable(module=a)
+            status = bpy.ops.preferences.addon_disable(module=a)
+
+            if 'FINISHED' not in status:
+                op.report({'ERROR'}, f"Error while disabling addon '{a}'!")
+                continue
+        
         mods = []
         for mod_name, mod in sys.modules.items():
             if a in mod_name:
                 mods.append(mod_name)
         for mod in mods:
             del sys.modules[mod]
-        bpy.ops.preferences.addon_enable(module=a)
-    os.chdir(cwd)
+        
+        status = bpy.ops.preferences.addon_enable(module=a)
+
+        if 'FINISHED' not in status:
+            op.report({'ERROR'}, f"Error while enabling addon '{a}'!")
+            continue
+        
+        # Restore preferences
+        if addon_prefs:
+            for p, v in addon_prefs.items():
+                bpy.context.preferences.addons[a].preferences[p] = v
+
+        op.report({'INFO'}, f"Updated and reloaded addon '{a}'.")
 
 
 class ReloadEnabledAddons(bpy.types.Operator):
@@ -81,8 +131,9 @@ class LoadAddons(bpy.types.Operator):
 class ReloadAddonsPrefs(bpy.types.AddonPreferences):
     bl_idname = __name__
     source_dir: bpy.props.StringProperty(
-        name="Source Dir",
-        subtype="FILE_PATH",
+        name="Source Directory",
+        description="The directory containing the source of the addons to reload as python files or directories containing each a python module for the addon",
+        subtype="DIR_PATH",
         default=str(Path.home()/'src'/'Blender')
     )
 
@@ -113,10 +164,6 @@ def unregister():
 
 
 if __name__ == "__main__":
-    import shutil
-
     script_path = Path(__file__)
-    addons_path = Path(bpy.utils.script_path_user()) / 'addons'
-    addons_path.mkdir(exist_ok=True)
-    shutil.copy2(script_path, addons_path)
-    print(f'Installed reload_addons script into {addons_path}')
+    bpy.ops.preferences.addon_install(filepath=str(script_path))
+    print(f'Installed reload_addons addon for Blender')
